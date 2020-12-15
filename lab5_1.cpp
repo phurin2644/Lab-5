0b090f8807e110ecd69eb389f24ff9e6c28e8bd4
phurin2644 <phus.2644@gmail.com>
2020-12-15 16:45:54 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
+
 int main(){
+    int N;
+    int x = 0;
+    int y = 0;
+
     cout << "Enter an integer: ";
+    cin >> N;//Take Input
+    while(N != 0){
+        if(N%2 == 0){//Check Even or Odd
+            x++;
+        }else{
+            y++;
+        }
+        cout << "Enter an integer: ";
+        cin >> N;
+    }
     cout << "#Even numbers = ";
+    cout << x << endl;
     cout << "#Odd numbers = ";
+    cout << y;
+    
     return 0;
 }
